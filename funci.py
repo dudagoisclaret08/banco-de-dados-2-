e67395ec48d43e33ccaa18cde0ffe1f72542c67e
dudagoisclaret08 <dudagois86@gmail.com>
2025-08-28 15:53:05 -0300

    Update funci.py

--- funci.py
+++ funci.py
@@ -1,107 +1,107 @@
 ##atvdd1 
-# import sqlite3 as funcionarios
+import sqlite3 as funcionarios
 
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
 
-# cursor.execute("UPDATE funcionarios SET salario = salario * 1.10 WHERE departamento = 'TI'")
-# banco.commit()  
+cursor.execute("UPDATE funcionarios SET salario = salario * 1.10 WHERE departamento = 'TI'")
+banco.commit()  
   
-# cursor.execute("SELECT nome, departamento, salario FROM funcionarios")
-# funcionarios = cursor.fetchall()
+cursor.execute("SELECT nome, departamento, salario FROM funcionarios")
+funcionarios = cursor.fetchall()
 
-# banco.close()
-
-
-
-##atvdd2 
-# import sqlite3 as funcionarios
-
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
-
-# cursor.execute("UPDATE funcionarios SET telefone = '(11) 98888-7777' WHERE cpf = '15967994960'")
-
-# banco.commit()
-# print("Telefone atualizado com sucesso!")
-
-# banco.close()
+banco.close()
 
 
 
-#atvdd3
-# import sqlite3 as funcionarios
+#atvdd2 
+import sqlite3 as funcionarios
 
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
 
-# cursor.execute("UPDATE funcionarios SET departamento = 'RH' WHERE pnome = 'Ana'")
+cursor.execute("UPDATE funcionarios SET telefone = '(11) 98888-7777' WHERE cpf = '15967994960'")
 
-# banco.commit()
-# print("Departamento atualizado com sucesso!")
+banco.commit()
+print("Telefone atualizado com sucesso!")
 
-# banco.close()
+banco.close()
+
+
+
+###atvdd3
+import sqlite3 as funcionarios
+
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
+
+cursor.execute("UPDATE funcionarios SET departamento = 'RH' WHERE pnome = 'Ana'")
+
+banco.commit()
+print("Departamento atualizado com sucesso!")
+
+banco.close()
 
 
 #atvdd4
-# import sqlite3 as funcionarios
+import sqlite3 as funcionarios
 
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
 
-# cursor.execute("UPDATE funcionarios SET cargo = 'Analista' WHERE cargo = 'Estagiário' ")
+cursor.execute("UPDATE funcionarios SET cargo = 'Analista' WHERE cargo = 'Estagiário' ")
 
-# banco.commit()
+banco.commit()
 
-# print("Cargo atualizado de Estagiário para Analista com sucesso!")
+print("Cargo atualizado de Estagiário para Analista com sucesso!")
 
-# banco.close()
+banco.close()
 
 
 # ##atvdd5
-# import sqlite3 as funcionarios
+import sqlite3 as funcionarios
 
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
 
-# cursor.execute("DELETE FROM funcionarios WHERE idade < 20")
+cursor.execute("DELETE FROM funcionarios WHERE idade < 20")
 
-# banco.commit()
+banco.commit()
 
-# print("Registros de funcionários com idade menor que 20 foram removidos com sucesso!")
+print("Registros de funcionários com idade menor que 20 foram removidos com sucesso!")
 
-# banco.close()
+banco.close()
 
 ##atvdd6
-# import sqlite3 as funcionarios
+import sqlite3 as funcionarios
 
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
 
-# cursor.execute("DELETE FROM funcionarios WHERE pnome = 'Victor'")
+cursor.execute("DELETE FROM funcionarios WHERE pnome = 'Victor'")
 
-# banco.commit()
+banco.commit()
 
-# print("Funcionário Victor removido com sucesso!")
+print("Funcionário Victor removido com sucesso!")
 
-# banco.close()
+banco.close()
 
 
 
 ##atvdd7
-# import sqlite3 as funcionarios
+import sqlite3 as funcionarios
 
-# banco = funcionarios.connect('ricardaofather.db')
-# cursor = banco.cursor()
+banco = funcionarios.connect('ricardaofather.db')
+cursor = banco.cursor()
 
 
-# cursor.execute("DELETE FROM funcionarios WHERE departamento = 'Marketing'")
+cursor.execute("DELETE FROM funcionarios WHERE departamento = 'Marketing'")
 
-# banco.commit()
+banco.commit()
 
-# print("Funcionários do departamento Marketing removidos com sucesso!")
+print("Funcionários do departamento Marketing removidos com sucesso!")
 
-# banco.close()
+banco.close()
 
 
 ##atvdd8
@@ -128,5 +128,6 @@ if cpf_duplicado:
         print("Nenhum registro encontrado para deletar.")
 else:
     print("Nenhum CPF duplicado encontrado.")
+
 
 banco.close()
